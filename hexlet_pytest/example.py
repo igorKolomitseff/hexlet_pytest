b1a79d69f5a5b2170401fde231d1826f6cac056e
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-08-02 14:36:49 +0300

    Add function reverse() to the example.py

---
+++ hexlet_pytest/example.py
@@ -0,0 +1,11 @@
+def reverse(string: str) -> str:
+    """Returns reversed string.
+
+    Args:
+        string: Any string.
+
+    Returns:
+        Reversed string.
+    """
+    
+    return string[::-1]
